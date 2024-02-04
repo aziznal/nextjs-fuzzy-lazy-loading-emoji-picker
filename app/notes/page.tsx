a53eeb53ab3befe9c4d80361440d5823d4c95e8b
Aziz <aziznal.dev@gmail.com>
2024-02-04 16:48:23 +0300

    create basic emoji list with filter

---
+++ app/notes/page.tsx
@@ -0,0 +1,11 @@
+export default function Notes() {
+  return (
+    <div className="h-full flex justify-center flex-col w-full sm:w-[600px] mx-auto">
+      <h1 className="text-2xl text-slate-700">Notes</h1>
+
+      <ul className="list-decimal pl-4">
+        <li>Finding a full emoji database is freaking hard.</li>
+      </ul>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,40 @@
+"use client";
+
+import * as emoji from "node-emoji";
+import { useState } from "react";
+
+const allEmojies = emoji.search("");
+
 export default function Home() {
+  const [currentEmojies, setCurrentEmojies] = useState([...allEmojies]);
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const search = e.target.value;
+
+    if (search === "") {
+      setCurrentEmojies(allEmojies);
+      return;
+    }
+
+    const result = emoji.search(search);
+
+    setCurrentEmojies(result);
+  };
+
   return (
-    <div className="h-full flex justify-center items-center">Hello World!</div>
+    <div className="h-full justify-center items-center flex flex-wrap text-4xl">
+      <input
+        type="text"
+        placeholder="Search for an emoji"
+        className="w-full p-2"
+        onChange={handleSearch}
+      />
+
+      {currentEmojies.map((e) => (
+        <span key={e.emoji} className="px-2">
+          {e.emoji}
+        </span>
+      ))}
+    </div>
   );
 }
